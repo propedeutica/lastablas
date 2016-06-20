5a433dc5e3c01f52abb7663736d98e754aaf0e39
root <catalinfira@hotmail.com>
2016-06-20 10:29:03 +0200

    Removed unwanted config from << spec_helper.rb >>.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,12 @@ RSpec.configure do |config|
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
   end
-
+  
+  #config.color = true
+  # Use color not only in STDOUT but also in pagers and files
+  #config.tty = true
+  # Use the specified formatter
+  #config.formatter = :documentation # :progress, :html, :textmate
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
