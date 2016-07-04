8587c56b0e0e80b6a69b0a86739d38f00e422339
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-07-04 10:10:30 +0200

    Rubocopped (almost) all the code.

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -2,71 +2,65 @@ require 'rails_helper'
 
 RSpec.describe AssignmentsController, type: :controller do
   context "invalid " do
-    let(:offspring) { FactoryGirl.build(:offspring)}
-    let(:shift) { FactoryGirl.build(:shift)}
-    
+    let(:offspring) { FactoryGirl.build(:offspring) }
+    let(:shift) { FactoryGirl.build(:shift) }
+
     describe "Offspring," do
-      it "should not allow an offspring without first name"  do
+      it "should not allow an offspring without first name" do
         offspring.first_name = nil
         expect(offspring).not_to be_valid
       end
-      
-      it "should not allow an offspring without user"  do
+
+      it "should not allow an offspring without user" do
         offspring.user = nil
         expect(offspring).not_to be_valid
       end
-      
-      it "should not allow an offspring without last name"  do
+
+      it "should not allow an offspring without last name" do
         offspring.last_name = nil
         expect(offspring).not_to be_valid
       end
-      
-      it "should not allow an offspring without grade"  do
+
+      it "should not allow an offspring without grade" do
         offspring.grade = nil
         expect(offspring).not_to be_valid
       end
     end
-    
-        
-    describe "Shift," do
 
-      it "should not allow a shift without day_of_week"  do
+    describe "Shift," do
+      it "should not allow a shift without day_of_week" do
         shift.day_of_week = nil
         expect(shift).not_to be_valid
       end
-      
-      it "should not allow a shift without start_time"  do
+      it "should not allow a shift without start_time" do
         shift.start_time = nil
         expect(shift).not_to be_valid
       end
-      
-      it "should not allow a shift without end_time"  do
+      it "should not allow a shift without end_time" do
         shift.end_time = nil
         expect(shift).not_to be_valid
       end
-      it "should not allow a shift without prebooked"  do
+      it "should not allow a shift without prebooked" do
         expect{
-
-          shift.prebooked = nil 
+          shift.prebooked = nil
           shift.valid?
-          }.to raise_exception("nil can't be coerced into Fixnum")
+        }.to raise_exception("nil can't be coerced into Fixnum")
       end
     end
   end
 
   context "valid offspring and/or shift" do
-    let(:offspring) { FactoryGirl.build(:offspring)}
-    let(:shift) { FactoryGirl.build(:shift)}
+    let(:offspring) { FactoryGirl.build(:offspring) }
+    let(:shift) { FactoryGirl.build(:shift) }
     it "should not allow a shift to be assigned when there are no empty places" do
-      shift.prebooked=shift.room.capacity
-      offspring.shift=shift
+      shift.prebooked = shift.room.capacity
+      offspring.shift = shift
       expect(offspring).not_to be_valid
     end
   end
 
   describe "GET #new" do
-
-    let(:user) {FactoryGirl.create(:user)}
+    let(:user) { FactoryGirl.create(:user) }
     it "returns http success" do
       sign_in user
       get :new
