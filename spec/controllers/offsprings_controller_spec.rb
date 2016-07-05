e2aae58c33b46c0c1127c13b980f824577a6d000
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-07-05 11:31:50 +0200

    Refactoring of tests, completion of some tests and addition of new pendings

--- spec/controllers/offsprings_controller_spec.rb
+++ spec/controllers/offsprings_controller_spec.rb
@@ -2,57 +2,53 @@ require 'rails_helper'
 include Devise::TestHelpers # this should work without this
 
 RSpec.describe OffspringsController, type: :controller do
-  describe "get INDEX" do
-    before :all do
-      3.times do
-        FactoryGirl.create(:offspring)
-      end
+  context "When not authenticated," do
+    it "does not allow to create" do
+      expect {
+        post :create, offspring: {first_name: "pepe", last_name: "kata", grade: :primary_first}
+      }.to raise_error(NoMethodError, "undefined method `authenticate!' for nil:NilClass")
     end
 
-    context "When not authenticated" do
-      # pending "redirects to authentication"
-      # You are not supposed to test an operation performed by another controller in offspring.
-      # We suggest to do this test in static_pages_controller_spec.rb
+    it "does not allow access new" do
+      expect{
+        get :new, {}
+      }.to raise_error(NoMethodError, "undefined method `authenticate!' for nil:NilClass")
     end
-    context "When authenticated" do
-      # pending "shows index of offspring"
-      # You are not supposed to test an operation performed by another controller in offspring.
-      # We suggest to do this test in static_pages_controller_spec.rb
+
+    let(:user) { FactoryGirl.create(:user) }
+    let(:off) { FactoryGirl.create(:offspring) }
+    it "does not allow to destroy" do
+      user.offsprings << off
+      expect {
+        delete :destroy, id: off.id
+      }.to raise_error(NoMethodError, "undefined method `authenticate!' for nil:NilClass")
     end
   end
 
-  describe "get CREATE" do
+  context "When authenticated," do
     let(:user) { FactoryGirl.create(:user) }
-
-    context "When create new offspring" do
-      it "allows primary_first children" do
+    describe "#create" do
+      it "allows creation of primary_first children" do
         sign_in user
         expect {
           post :create, offspring: {first_name: "pepe", last_name: "kata", grade: :primary_first}
         }.to change(user.offsprings, :count).by(1)
       end
-
-      describe "and not in first grade" do
-        it "does not allow any other" do
-          sign_in user
-          Offspring.grades.keys.each do |i|
-            if i != 'primary_first'
-              expect{
-                post :create, offspring: {first_name: "pepe", last_name: "kata", grade: i}
-              }.to change(user.offsprings, :count).by(0)
-            end
-          end
-        end
-
-        it "redirects to /static_pages/intructions" do
-          sign_in user
-          Offspring.grades.keys.each do |i|
-            if i != 'primary_first'
+      it "does not allow any other and redirects" do
+        sign_in user
+        Offspring.grades.keys.each do |i|
+          if i != 'primary_first'
+            expect{
               post :create, offspring: {first_name: "pepe", last_name: "kata", grade: i}
-              expect(response).to redirect_to(static_pages_intructions_path)
-            end
+            }.to change(user.offsprings, :count).by(0)
+            expect(response).to redirect_to(static_pages_intructions_path)
           end
         end
+      end
+      pending "#new shows view and fields for new offspring"
+      describe "#destroy" do # We don't know the real functionality, we'll see how it works and adapt the test to it
+        pending "allows destroying your offsprings"
+        pending "does not allow destroyinh other's offsrpings"
       end
     end
   end
