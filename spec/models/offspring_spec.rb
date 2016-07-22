8c8973a0370cdf1c1b4739d208eb8e50e46bad5b
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-07-22 11:01:41 +0200

    Rubocoped spec for offspring model

--- spec/models/offspring_spec.rb
+++ spec/models/offspring_spec.rb
@@ -7,41 +7,40 @@ RSpec.describe Offspring, type: :model do
   end
 
   it "is invalid without a name" do
-    child = FactoryGirl.build(:offspring, first_name:nil)
+    child = FactoryGirl.build(:offspring, first_name: nil)
     expect(child).not_to be_valid
     child.valid?
     expect(child.errors[:first_name]).to include("no puede estar vacío")
-
   end
 
   it "is invalid without a surname" do
-    child = FactoryGirl.build(:offspring, last_name:nil)
+    child = FactoryGirl.build(:offspring, last_name: nil)
     expect(child).not_to be_valid
     child.valid?
     expect(child.errors[:last_name]).to include("no puede estar vacío")
   end
 
   it "is invalid wihout an course" do
-    child=FactoryGirl.build(:offspring, grade:nil)
+    child = FactoryGirl.build(:offspring, grade: nil)
     expect(child).not_to be_valid
     child.valid?
     expect(child.errors[:grade]).to include("no puede estar vacío")
   end
 
-  it 'is in any grade' do 
+  it 'is in any grade' do
     Offspring.grades.keys.each do |i|
-      child=FactoryGirl.build(:offspring, :grade => i)
+      child = FactoryGirl.build(:offspring, grade: i)
       expect(child).to be_valid
-    end 
-  end 
+    end
+  end
 
   it "is invalid without a user associated" do
-    child = FactoryGirl.build(:offspring, user:nil)
+    child = FactoryGirl.build(:offspring, user: nil)
     expect(child).not_to be_valid
   end
 
   it "is destroyed when the user associated is" do
     child = FactoryGirl.create(:offspring)
-    expect { child.user.destroy! }.to change{ Offspring.count}.by(-1)
+    expect { child.user.destroy! }.to change { Offspring.count }.by(-1)
   end
 end
