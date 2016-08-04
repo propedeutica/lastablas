6e79b0e7d13040fa900f087f15841a4efe0415da
Sergio Ocon <sergio.ocon@redhat.com>
2016-08-04 10:09:53 +0200

    improved the offspring specs and added a Guardfile

--- spec/models/offspring_spec.rb
+++ spec/models/offspring_spec.rb
@@ -1,44 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Offspring, type: :model do
+  let(:child) { FactoryGirl.build(:offspring) }
+
   it "has a valid factory" do
-    child = FactoryGirl.build(:offspring)
+    child.valid?
     expect(child).to be_valid
   end
 
   it "is invalid without a name" do
-    child = FactoryGirl.build(:offspring, first_name: nil)
-    expect(child).not_to be_valid
+    child.first_name = nil
     child.valid?
     expect(child.errors[:first_name]).to include("no puede estar vacío")
   end
 
   it "is invalid without a surname" do
-    child = FactoryGirl.build(:offspring, last_name: nil)
-    expect(child).not_to be_valid
+    child.last_name = nil
     child.valid?
     expect(child.errors[:last_name]).to include("no puede estar vacío")
   end
 
   it "should have the same surname than the brothers" do
-    child = FactoryGirl.create(:offspring)
+    child.save
     brother = FactoryGirl.build(:offspring, user: child.user, last_name: "aaassssddd")
     brother.valid?
     expect(brother.errors[:last_name]).to include("tiene que coincidir con el de sus hermanos")
   end
 
   it "is invalid wihout a grade" do
-    child = FactoryGirl.build(:offspring, grade: nil)
-    expect(child).not_to be_valid
+    child.grade = nil
     child.valid?
     expect(child.errors[:grade]).to include("no puede estar vacío")
   end
 
-  pending "grades should belong to the grade list"
+  
+  it "is invalid with grades that don't belong to the grade list" do
+    expect{ child.grade = 100 }.to raise_error(ArgumentError).with_message(/is not a valid grade/)
+  end
 
   it "is invalid without a user associated" do
-    child = FactoryGirl.build(:offspring, user: nil)
-    expect(child).not_to be_valid
+    child.user=nil
+    child.valid?
+    expect(child.errors[:user]).to include("no puede faltar")
   end
 
   it "is destroyed when the user associated is" do
