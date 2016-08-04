d2e36aa2d8e332899d147531cd5498673357d937
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-08-04 13:19:21 +0200

    Manually rebase for shift model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,15 +1,18 @@
 class Shift < ActiveRecord::Base
   belongs_to :room
-  has_many :offsprings, dependent: :nullify
-  validates :day_of_week,
-            presence:     true,
-            numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 7 }
-  validates :start_time, presence: true
-  validates :end_time, presence: true
-  validates :prebooked, presence: true, numericality: { only_integer: true }
-  validates_each :prebooked do |shift, _room|
-    shift.errors.add(:shift, "no puede ser mayor que la capacidad de la sala") unless shift.sites_available?
+  has_many :offsprings, :dependent => :nullify
+  validates :day_of_week, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 1,
+    :less_than_or_equal_to => 7 }
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
+  validates :prebooked, :presence => true, :numericality => { :only_integer => true }
+  validates_each :prebooked do |shift|
+    shift.errors.add(:shift, "no puede ser mayor que la capacidad de la sala") if shift.nil? && !shift.sites_available?
   end
+  validate :sites_available_gt_0
+  validate :regexed_time
+  validate :time_continuity
+  before_save :always_same_week
 
   def sites_available?
     room.capacity - prebooked - offsprings.count > 0
@@ -17,5 +20,40 @@ class Shift < ActiveRecord::Base
 
   def sites_available
     room.capacity - prebooked - offsprings.count
+  end
+
+  private
+
+  def sites_available_gt_0
+    return true if sites_available? && sites_available >= 0
+    errors.add(:shift, 'Sites available under 0')
+    false
+  end
+
+  def regexed_time
+    regex = /([01]\d|2[0123]):[012345]\d$/
+    if start_time !~ regex
+      errors.add(:start_time, 'Wrong start time')
+      return false
+    end
+
+    if end_time !~ regex
+      errors.add(:end_time, 'Wrong end time')
+      return false
+    end
+    true
+  end
+
+  def time_continuity
+    return true if start_time < end_time
+    errors.add(:end_time, 'End time is ahead of start time')
+    false
+  end
+
+  def always_same_week
+    # day=self.day_of_week
+    # day=1 unless (1..7).include?(day)
+    start_time || "00:00"
+    end_time || "01:00"
   end
 end
