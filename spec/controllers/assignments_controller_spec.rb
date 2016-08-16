f8b1b0e83c69ff0e3d7fc7f2f5ec68ca692d81ad
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-08-16 12:18:37 +0200

    Improvement and fixes in assigment controller spec

--- spec/controllers/assignments_controller_spec.rb
+++ spec/controllers/assignments_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AssignmentsController, type: :controller do
   context "invalid " do
-    let(:offspring) { FactoryGirl.build(:offspring) }
-    let(:shift) { FactoryGirl.build(:shift) }
+    let(:offspring) { FactoryGirl.build_stubbed(:offspring) }
+    let(:shift) { FactoryGirl.build_stubbed(:shift) }
 
     describe "Offspring," do
       it "should not allow an offspring without first name" do
@@ -59,22 +59,70 @@ RSpec.describe AssignmentsController, type: :controller do
     end
   end
 
-  context "valid offspring and/or shift" do
-    let(:offspring) { FactoryGirl.build(:offspring) }
-    let(:shift) { FactoryGirl.build(:shift) }
-    it "should not allow a shift to be assigned when there are no empty places" do
-      shift.prebooked = shift.room.capacity
-      offspring.shift = shift
-      expect(offspring).not_to be_valid
+  context "not authnticated" do
+    let(:offspring) { FactoryGirl.build_stubbed(:offspring) }
+    let(:shift) { FactoryGirl.build_stubbed(:shift) }
+
+    it "GET #new fails" do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    it "POST #create fails" do
+      post :create, format: offspring.id, shift: shift.id
+      expect(response.response_code).to eq 401
+    end
+    it "DELETE #destroys fails" do
+      post :destroy, id: offspring.id
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
-  describe "GET #new" do
-    let(:user) { FactoryGirl.create(:user) }
-    it "returns http success" do
-      sign_in user
-      get :new
-      expect(response).to have_http_status(:success)
+  context "valid offspring and/or shift" do
+    let(:offspring) { FactoryGirl.build_stubbed(:offspring) }
+    let(:shift) { FactoryGirl.build_stubbed(:shift) }
+
+    it "should allow to assign to a valid shift" do
+      shift.prebooked = 0
+      offspring.shift = shift
+      offspring.valid?
+      expect(offspring).to be_valid
+    end
+
+    it "should not allow a shift to be assigned when there are no empty places" do
+      shift.prebooked = shift.room.capacity
+      offspring.shift = shift
+      offspring.valid?
+      expect(offspring).not_to be_valid
+      expect(offspring.errors[:shift]).to include "no hay espacio"
+    end
+
+    describe "authnticated" do
+      let(:user) { FactoryGirl.create(:user) } # Necessary creat to sign in
+      let(:offspring) { FactoryGirl.create(:offspring) }
+      let(:shift) { FactoryGirl.create(:shift) }
+      before(:each) do
+        sign_in user
+      end
+
+      it "GET #new returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
+      end
+
+      it "POST #create returns" do
+        expect do
+          post :create,  format: offspring.id, shift: shift.id
+        end.to change(shift.offsprings, :count).by(1)
+        expect(response).to redirect_to(root_path)
+      end
+      it "DELETE #destroy returns" do
+        offspring.shift = shift
+        offspring.save
+        expect do
+          delete :destroy, id: offspring.id
+        end.to change(shift.offsprings, :count).by(-1)
+        expect(response).to redirect_to(new_assignment_path + '.' + offspring.id.to_s)
+      end
     end
   end
 end
