bac6337f34f0d47d7a26a0614e32f45a2d96fb1d
root <catalinfira@hotmail.com>
2016-08-22 12:23:13 +0200

    adding configs to fix access patternfly/bower jquery scrips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-
-//
+//= require_tree .
+//= require_self
 // Require elements for patternfly to work
 //
 //= require bootstrap
 //= require patternfly
-
-
-
-
+//= require bootstrap-switch
+//= require datatables
+//= require datatables.net-buttons-bs
