653e5963b363cfcc53f9d7c046a495d487f3e4e2
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-08-26 09:26:07 +0200

    Manual rebase

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -5,12 +5,16 @@ class AssignmentsController < ApplicationController
   end
 
   def create
-    Offspring.transaction do
+    if admin_allows_changes?
+      Offspring.transaction do
       of = Offspring.lock.find_by_id(params["format"])
       of.shift = Shift.find_by_id(params["shift"])
       of.save
-      redirect_to root_url
     end
+    else
+      flash[:alert] = "No se puedo añadir al niño en este momento, espere."
+    end
+    redirect_to root_url
   end
 
   def destroy
@@ -18,5 +22,11 @@ class AssignmentsController < ApplicationController
     of.shift = nil
     of.save
     redirect_to new_assignment_path(of)
+  end
+
+  private
+
+  def admin_allows_changes?
+    !ApplicationHelper.status_lock?
   end
 end
