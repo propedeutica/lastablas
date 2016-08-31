4e234ccb7844de4f49ba4e4217b87d685a52bfd4
alexvkcr <manuel.diaz-ext@neartechnologies.com>
2016-08-31 09:51:22 +0200

    Fix for rubocop

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -5,17 +5,8 @@ class AssignmentsController < ApplicationController
     @rooms = Room.all
   end
 
-  def create    
-    if access_control?
-      Offspring.transaction do
-        of = Offspring.lock.find_by_id(params["format"])
-        of.shift = Shift.find_by_id(params["shift"])
-        of.save
-      end
-      flash[:success] = I18n.t("assignment_create_success", scope: SCOPE)
-    else
-      flash[:alert] = I18n.t("admin_locked_create", scope: SCOPE)
-    end
+  def create
+    access_control? ? create_assignment : flash[:alert] = I18n.t("admin_locked_create", scope: SCOPE)
     redirect_to root_url
   end
 
@@ -29,10 +20,19 @@ class AssignmentsController < ApplicationController
   private
 
   def access_control?
-    if current_user.admin? 
+    if current_user.admin?
       true
     else
       !ApplicationHelper.status_lock?
+    end
+  end
+
+  def create_assignment
+    Offspring.transaction do
+      of = Offspring.lock.find_by_id(params["format"])
+      of.shift = Shift.find_by_id(params["shift"])
+      of.save
+      flash[:success] = I18n.t("assignment_create_success", scope: SCOPE)
     end
   end
 end
